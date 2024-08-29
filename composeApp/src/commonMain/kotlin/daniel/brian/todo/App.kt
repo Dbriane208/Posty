1f1e8e46df34dca9f47db94182bdac8325499cac
Daniel Brian <db9755949@gmail.com>
2024-08-29 15:20:59 +0300

    Feat: AlertDialog to create new posts

--- composeApp/src/commonMain/kotlin/daniel/brian/todo/App.kt
+++ composeApp/src/commonMain/kotlin/daniel/brian/todo/App.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
@@ -13,8 +14,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import daniel.brian.todo.components.CreatePost
 import daniel.brian.todo.components.PostComponent
+import daniel.brian.todo.model.Post
 import daniel.brian.todo.viewmodel.PostViewModel
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
@@ -25,26 +32,55 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme {
         Surface(
-            modifier = Modifier.verticalScroll(rememberScrollState())
+            modifier = Modifier
                 .fillMaxSize(),
         ) {
-
-            val postViewModel = getViewModel(Unit, viewModelFactory { PostViewModel() })
-            val uiState by postViewModel.uiState.collectAsState()
-
-            LaunchedEffect(postViewModel){
-                postViewModel.updatePosts()
-            }
-
-            Column {
-                uiState.posts.forEach{post ->
-                    PostComponent(
-                        modifier = Modifier,
-                        post =post
-                    )
-                }
-            }
+            PostScreen()
         }
 
     }
+}
+
+@Composable
+fun PostScreen() {
+    val postViewModel = getViewModel(Unit, viewModelFactory { PostViewModel() })
+    val uiState by postViewModel.uiState.collectAsState()
+
+    var showCreateDialog by remember { mutableStateOf(false) }
+    var selectedPost by remember { mutableStateOf<Post?>(null) }
+
+    LaunchedEffect(postViewModel){
+        postViewModel.updatePosts()
+    }
+
+        LazyColumn {
+            items(uiState.posts){post ->
+                PostComponent(
+                    modifier = Modifier,
+                    post = post,
+                    onMoreVertClick = {
+                        selectedPost = post
+                        showCreateDialog = true
+                    }
+                )
+
+            }
+        }
+
+
+    if(selectedPost != null && showCreateDialog){
+        CreatePost(
+            post = selectedPost!!,
+            onDismiss = {
+                showCreateDialog = false
+                selectedPost = null
+            },
+            onConfirm = { newPost->
+                // Handle the updated post here
+                showCreateDialog = false
+                selectedPost = null
+            }
+        )
+    }
+
 }
--- composeApp/src/commonMain/kotlin/daniel/brian/todo/components/PostComponent.kt
+++ composeApp/src/commonMain/kotlin/daniel/brian/todo/components/PostComponent.kt
@@ -1,18 +1,35 @@
 package daniel.brian.todo.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import daniel.brian.todo.model.Post
@@ -20,7 +37,8 @@ import daniel.brian.todo.model.Post
 @Composable
 fun PostComponent(
     modifier: Modifier = Modifier,
-    post: Post
+    post: Post,
+    onMoreVertClick: () -> Unit
 ) {
     Card(
         modifier = modifier
@@ -29,6 +47,20 @@ fun PostComponent(
         elevation = 10.dp,
         shape = RoundedCornerShape(10.dp)
     ) {
+        Row(
+            modifier = modifier,
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = {
+                onMoreVertClick()
+            } ){
+                Icon(
+                    Icons.Default.MoreVert,
+                    contentDescription = null
+                )
+            }
+        }
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
@@ -65,4 +97,56 @@ private fun InfoRow(
             overflow = TextOverflow.Ellipsis
         )
     }
+}
+
+@Composable
+fun CreatePost(
+    modifier: Modifier = Modifier,
+    post: Post,
+    onDismiss: () -> Unit,
+    onConfirm: (Post) -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = { onConfirm(post) }) {
+                Text("Create")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        },
+        title = { Text("Create Post") },
+        text = {
+            Column(
+                modifier = modifier.padding(8.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+
+                 OutlinedTextField(
+                     value = post.title.toString(),
+                     onValueChange = {  },
+                     label = { Text("Title") },
+                     keyboardOptions = KeyboardOptions.Default.copy(
+                         keyboardType = KeyboardType.Text,
+                         imeAction = ImeAction.Next
+                     )
+                 )
+                 OutlinedTextField(
+                     value = post.description.toString(),
+                     onValueChange = { },
+                     label = { Text("Description") },
+                     keyboardOptions = KeyboardOptions.Default.copy(
+                         keyboardType = KeyboardType.Text,
+                         imeAction = ImeAction.Next
+                     )
+                 )
+            }
+        },
+        modifier = Modifier
+            .clip(MaterialTheme.shapes.medium)
+            .background(MaterialTheme.colors.surface)
+    )
 }
